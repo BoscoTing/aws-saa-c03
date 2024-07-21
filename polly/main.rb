626d1e803763f1b8304997a1382e175e36dabd89
Hao Ting <boscoting0522@gmail.com>
2024-07-21 13:46:11 +0800

    feat: aws polly example

---
+++ polly/main.rb
@@ -0,0 +1,17 @@
+require 'aws-sdk-polly'
+require 'pry'
+
+client = Aws::Polly::Client.new
+
+resp = client.synthesize_speech({
+  engine: "standard",
+  output_format: "mp3", 
+  sample_rate: "8000", 
+  text: "All Gaul is divided into three parts", 
+  text_type: "text", 
+  voice_id: "Joanna", 
+})
+
+File.open('sample.mp3', 'wb') do |file|
+    file.write(resp.audio_stream.read)
+  end
